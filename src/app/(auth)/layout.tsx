43c9a02276fffb1fe28793584025fa0d9aee87c4
Vishal-770 <vishalpeace07@gmail.com>
2025-12-31 16:34:32 +0530

    refactor: remove ModeToggle component and related markup from AuthLayout

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,6 +1,5 @@
 import { redirect } from "next/navigation";
 import { getSession } from "@/src/lib/auth-server";
-import { ModeToggle } from "@/src/components/ModeToggle";
 
 export default async function AuthLayout({
   children,
@@ -14,13 +13,5 @@ export default async function AuthLayout({
     redirect("/");
   }
 
-  return (
-    <div className="relative min-h-svh">
-      {children}
-      {/* Theme toggle at bottom right */}
-      <div className="fixed bottom-6 right-6">
-        <ModeToggle />
-      </div>
-    </div>
-  );
+  return <div className="relative min-h-svh">{children}</div>;
 }
